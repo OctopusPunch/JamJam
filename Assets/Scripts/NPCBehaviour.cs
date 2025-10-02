4741bfd00a0dc6b388846e66f187291dd108404c
TwoDTools <TwoDTools@gmail.com>
2025-10-02 09:45:06 +0100

    Added mouse hover for stats, removed use of old input system for mouse detection and added to the NPC behaviour

--- Assets/Scripts/NPCBehaviour.cs
+++ Assets/Scripts/NPCBehaviour.cs
@@ -1,3 +1,4 @@
+using TMPro;
 using UnityEngine;
 
 public class NPCBehaviour : MonoBehaviour
@@ -15,12 +16,16 @@ public class NPCBehaviour : MonoBehaviour
     private GameObject eyeSprite;
 
     [SerializeField]
-    private GameObject resourceCount;
+    private TMP_Text resourceCountText;
+
+
 
     private void Start()
     {
         isTarget = false;
         eyeSprite.SetActive(isTarget);
+        SetResource();
+        HideResource();
     }
 
     void Update()
@@ -44,5 +49,39 @@ public class NPCBehaviour : MonoBehaviour
     public void SetAttribute(NPCAttributes attributes)
     {
         this.attributes = attributes;
+        SetResource();
+    }
+
+    private void OnMouseDown()
+    {
+        SetAsTarget(!IsTarget);
+
+        // push to some eye watch list
+    }
+
+    private void OnMouseEnter()
+    {
+        ShowReseource();
+    }
+
+    private void OnMouseExit()
+    {
+        HideResource();
+    }
+
+    void SetResource()
+    {
+        resourceCountText.text = "Water: " + attributes.waterResource.ToString();
+        resourceCountText.text += "<br>Food: " + attributes.foodResource.ToString();
+    }
+
+    void ShowReseource()
+    {
+        resourceCountText.gameObject.SetActive(true);
+    }
+
+    void HideResource()
+    {
+        resourceCountText.gameObject.SetActive(false);
     }
 }
