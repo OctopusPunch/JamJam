5c8b149ea754a18269e05378862a9a0ea31cc100
TwoDTools <TwoDTools@gmail.com>
2025-10-05 03:07:48 +0100

    added random dual + more randoms + dual solves

--- Assets/Scripts/WaveManager/DeckManager.cs
+++ Assets/Scripts/WaveManager/DeckManager.cs
@@ -69,8 +69,6 @@ public class DeckManager
 
     public void AddSolution(List<NPCAttributes> list)
     {
-        GenerateSingleTypeSolution(list);
-        return;
         // Max Solution 5
         if (currentWave < 4)
         {
@@ -81,48 +79,51 @@ public class DeckManager
         // Max Solution 7
         if(currentWave < 7)
         {
-
+            GenerateDoubleTypeSolution(list);
             return;
         }
 
         // Max Solution 8
         if(currentWave < 12)
         {
-
+            GenerateSingleTypeSolution(list);
             return;
         }
 
+        GenerateDoubleTypeSolution(list);
         // Max Solution 10
-        
+
     }
     public void AddRandomness(List<NPCAttributes> list)
     {
         GenerateRandomSingle(list, (NPCAttributes.ResourceType)Random.Range(0, 3));
         GenerateRandomSingle(list, (NPCAttributes.ResourceType)Random.Range(0, 3));
-        return;
         // Max NPCs 5
         if (currentWave < 4)
         {
-            GenerateRandomSingle(list, (NPCAttributes.ResourceType)Random.Range(0, 3));
-            GenerateRandomSingle(list, (NPCAttributes.ResourceType)Random.Range(0, 3));
             return;
         }
 
         // Max NPCs 7
         if (currentWave < 7)
         {
-
+            GenerateRandomDual(list, (NPCAttributes.ResourceType)Random.Range(0, 3), (NPCAttributes.ResourceType)Random.Range(0, 3));
+            GenerateRandomSingle(list, (NPCAttributes.ResourceType)Random.Range(0, 3));
             return;
         }
 
         // Max NPCs 8
         if (currentWave < 12)
         {
-
+            GenerateRandomDual(list, (NPCAttributes.ResourceType)Random.Range(0, 3),(NPCAttributes.ResourceType)Random.Range(0, 3));
+            GenerateRandomDual(list, (NPCAttributes.ResourceType)Random.Range(0, 3), (NPCAttributes.ResourceType)Random.Range(0, 3));
             return;
         }
 
         // Max NPCs 10
+        GenerateRandomDual(list, (NPCAttributes.ResourceType)Random.Range(0, 3), (NPCAttributes.ResourceType)Random.Range(0, 3));
+        GenerateRandomDual(list, (NPCAttributes.ResourceType)Random.Range(0, 3), (NPCAttributes.ResourceType)Random.Range(0, 3));
+        GenerateRandomSingle(list, (NPCAttributes.ResourceType)Random.Range(0, 3));
     }
 
     public void GenerateRandomSingle(List<NPCAttributes> list, NPCAttributes.ResourceType resourceType)
@@ -130,6 +131,40 @@ public class DeckManager
         NPCAttributes npc = ScriptableObject.CreateInstance<NPCAttributes>();
         npc.resourceType1 = resourceType;
         npc.resourceAmount1 = Random.Range(1, 4);
+
+        list.Add(npc);
+    }
+
+    public void GenerateRandomDual(List<NPCAttributes> list, NPCAttributes.ResourceType resourceType1, NPCAttributes.ResourceType resourceType2)
+    {
+        NPCAttributes npc = ScriptableObject.CreateInstance<NPCAttributes>();
+        npc.resourceType1 = resourceType1;
+        npc.resourceAmount1 = Random.Range(1, 4);
+
+        if(resourceType1 == resourceType2)
+        {
+
+            bool increment = Random.Range(0.0f, 1.0f) > .5f;
+            int resourceSelection = (int)resourceType2;
+            if (increment)
+            {
+                if (--resourceSelection < 0)
+                {
+                    resourceSelection = 2;
+                }
+            }
+            else
+            {
+                if (++resourceSelection > 2)
+                {
+                    resourceSelection = 0;
+                }
+            }
+            resourceType2 = (NPCAttributes.ResourceType)resourceSelection;
+        }
+
+        npc.resourceType2 = resourceType2;
+        npc.resourceAmount2 = Random.Range(1, 4);
 
         list.Add(npc);
     }
@@ -197,7 +232,141 @@ public class DeckManager
             targetGold -= val;
             list.Add(npc);
         }
+    }
+    public void GenerateDoubleTypeSolution(List<NPCAttributes> list)
+    {
+        int targetFood = TownResourceBehaviour.Instance.TargetFoodValue;
+        int targetGold = TownResourceBehaviour.Instance.TargetGoldValue;
+        int targetWater = TownResourceBehaviour.Instance.TargetWaterValue;
 
+
+        while(targetFood > 2 && targetGold > 2 && targetWater > 2)
+        {
+            NPCAttributes npc = ScriptableObject.CreateInstance<NPCAttributes>();
+
+            int selection = Random.Range(0, 3);
+            int val;
+            switch (selection)
+            {
+                case 0:
+                    val = Random.Range(3, targetFood + 1);
+                    npc.resourceType1 = NPCAttributes.ResourceType.Food;
+                    npc.resourceAmount1 = val;
+                    targetFood -= val;
+                    break;
+                case 1:
+                    val = Random.Range(3, targetWater + 1);
+                    npc.resourceType1 = NPCAttributes.ResourceType.Water;
+                    npc.resourceAmount1 = val;
+                    targetWater -= val;
+                    break;
+                default:
+                case 2:
+                    val = Random.Range(3, targetGold + 1);
+                    npc.resourceType1 = NPCAttributes.ResourceType.Gold;
+                    npc.resourceAmount1 = val;
+                    targetGold -= val;
+                    break;
+            }
+
+            bool increment = Random.Range(0.0f, 1.0f) > .5f;
+            
+            if(increment)
+            {
+                if(--selection < 0)
+                {
+                    selection = 2;
+                }
+            }
+            else
+            {
+                if(++selection > 2)
+                {
+                    selection = 0;
+                }
+            }
+
+            switch (selection)
+            {
+                case 0:
+                    val = Random.Range(3, targetFood + 1);
+                    npc.resourceType2 = NPCAttributes.ResourceType.Food;
+                    npc.resourceAmount2 = val;
+                    targetFood -= val;
+                    break;
+                case 1:
+                    val = Random.Range(3, targetWater + 1);
+                    npc.resourceType2 = NPCAttributes.ResourceType.Water;
+                    npc.resourceAmount2 = val;
+                    targetWater -= val;
+                    break;
+                default:
+                case 2:
+                    val = Random.Range(3, targetGold + 1);
+                    npc.resourceType2 = NPCAttributes.ResourceType.Gold;
+                    npc.resourceAmount2 = val;
+                    targetGold -= val;
+                    break;
+            }
+            list.Add(npc);
+        }
+
+        while(targetFood >= 1)
+        {
+            NPCAttributes npc = ScriptableObject.CreateInstance<NPCAttributes>();
+            int val;
+
+            if (targetFood < 3)
+            {
+                val = targetFood;
+            }
+            else
+            {
+                val = Random.Range(3, targetFood + 1);
+            }
+            npc.resourceType1 = NPCAttributes.ResourceType.Food;
+            npc.resourceAmount1 = val;
+            targetFood -= val;
+            list.Add(npc);
+        }
+
+        while(targetWater >= 1)
+        {
+            NPCAttributes npc = ScriptableObject.CreateInstance<NPCAttributes>();
+            int val;
+
+            if (targetWater < 3)
+            {
+                val = targetWater;
+            }
+            else
+            {
+                val = Random.Range(3, targetWater + 1);
+            }
+            npc.resourceType1 = NPCAttributes.ResourceType.Water;
+            npc.resourceAmount1 = val;
+            targetWater -= val;
+            list.Add(npc);
+        }
+
+        while(targetGold >= 1)
+        {
+            NPCAttributes npc = ScriptableObject.CreateInstance<NPCAttributes>();
+            int val;
+
+            if (targetGold < 3)
+            {
+                val = targetGold;
+            }
+            else
+            {
+                val = Random.Range(3, targetGold + 1);
+            }
+            npc.resourceType1 = NPCAttributes.ResourceType.Gold;
+            npc.resourceAmount1 = val;
+            targetGold -= val;
+            list.Add(npc);
+        }
     }
 
     public void SpawnWave(List<NPCAttributes> pulledSolution)
